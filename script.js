86518f434c4ebfd9ee40afac31e8f09caed81db4
kazzical <133119885+kazzical@users.noreply.github.com>
2024-05-06 19:42:45 +0000

    learned the .join method and learned about .reverse

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let vegetables = ["squash", "broccoli", "carrots", "celery"];
+vegetables.push("tomatoes");
+vegetables.unshift();
+let fruits =["grapes","strawberries","blueberries","plums","bananas"]
 
+let produce= vegetables.concat(fruits);
+produce.sort();
+produce.reverse();
+console.log(produce);
 
-
-
-
+let produceString=[];
+console.log(produce.join());
 /* DO NOT CHANGE BEYOND THIS LINE */
 
 const body = document.querySelector('body');
